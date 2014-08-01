da68203a39ee3d9a06d8763488e1488421a3a054
Sylence <sylence@btbsoft.org>
2014-08-02 00:55:57 +0200

    Added tests for the WriteToFile task

---
+++ BumpVersion/BumpVersion.Tests/WriteToFileTests.cs
@@ -0,0 +1,84 @@
+﻿// $Id$
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BumpVersion.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BumpVersion.Tests
+{
+	[TestClass]
+	public class WriteToFileTests
+	{
+		[TestMethod]
+		public void BumpExceptionTest()
+		{
+			using( ShimsContext.Create() )
+			{
+				System.IO.Fakes.ShimFile.WriteAllTextStringString = ( path, content ) =>
+				{
+					throw new IOException( "test exception" );
+				};
+
+				Dictionary<string, string> settings = new Dictionary<string, string>
+				{
+					{"files", "exception.txt"}
+				};
+
+				WriteToFile task = new WriteToFile( settings );
+				OperationResult result = task.Bump( new Version( 1, 0 ) );
+
+				Assert.IsFalse( result.IsSuccess );
+				Assert.IsTrue( result.ToString( true ).Contains( "test exception" ) );
+			}
+		}
+
+		[TestMethod]
+		public void BumpTest()
+		{
+			Dictionary<string, string> settings = new Dictionary<string, string>
+			{
+				{"files", "out1.txt;out2.txt"}
+			};
+			WriteToFile task = new WriteToFile( settings );
+			OperationResult result = task.Bump( new Version( 1, 0 ) );
+
+			Assert.IsTrue( result.IsSuccess );
+
+			Assert.IsTrue( File.Exists( "out1.txt" ) );
+			Assert.IsTrue( File.Exists( "out2.txt" ) );
+
+			Assert.AreEqual( "1.0", File.ReadAllText( "out1.txt" ) );
+			Assert.AreEqual( "1.0", File.ReadAllText( "out2.txt" ) );
+		}
+
+		[TestMethod]
+		public void ValidateTest()
+		{
+			Dictionary<string, string> settings = new Dictionary<string, string>();
+			WriteToFile task = new WriteToFile( settings );
+			OperationResult validation = task.Validate();
+
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.ToString( true ).Contains( "No files given" ) );
+
+			settings["files"] = string.Empty;
+			task = new WriteToFile( settings );
+			validation = task.Validate();
+
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.ToString( true ).Contains( "No files given" ) );
+
+			settings["files"] = "out.txt";
+			task = new WriteToFile( settings );
+			validation = task.Validate();
+
+			Assert.IsTrue( validation.IsSuccess );
+		}
+	}
+}

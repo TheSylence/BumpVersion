95e7ae2748d3c8054c66075d542be690aa1cf78a
Sylence <sylence@btbsoft.org>
2014-11-11 11:34:25 +0100

    First code for ImageWatermark task. Still needs some tests and tweaks

---
+++ BumpVersion/BumpVersion.Tests/Tasks/ImageWatermarkTests.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using BumpVersion.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BumpVersion.Tests.Tasks
+{
+	[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+	[TestClass]
+	public class ImageWatermarkTests
+	{
+		[TestMethod]
+		public void BumpTest()
+		{
+			Dictionary<string, string> variables = new Dictionary<string, string>();
+			Dictionary<string, string> settings = new Dictionary<string, string>();
+			settings.Add( "source", "SourceImage.png" );
+			settings.Add( "dest", "default.png" );
+			settings.Add( "rect", "0;0" );
+
+			ImageWatermark task = new ImageWatermark( settings, variables );
+			OperationResult validation = task.Bump( new Version( 1, 0 ), new Version( 1, 1 ) );
+			Assert.IsTrue( validation.IsSuccess );
+			CompareImageWithReference( "default.png" );
+
+			settings.Add( "color", "red" );
+			settings["dest"] = "defaultRed.png";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Bump( new Version( 1, 0 ), new Version( 1, 1 ) );
+			Assert.IsTrue( validation.IsSuccess );
+			CompareImageWithReference( "defaultRed.png" );
+
+			settings["rect"] = "100;10;10;20";
+			settings["dest"] = "rectangleCut.png";
+			settings.Remove( "color" );
+			task = new ImageWatermark( settings, variables );
+			validation = task.Bump( new Version( 1, 0 ), new Version( 1, 1 ) );
+			Assert.IsTrue( validation.IsSuccess );
+			CompareImageWithReference( "rectangleCut.png" );
+
+			settings["rect"] = "0;0";
+			settings["dest"] = "font.png";
+			settings.Add( "font", "Consolas:13" );
+			task = new ImageWatermark( settings, variables );
+			validation = task.Bump( new Version( 1, 0 ), new Version( 1, 1 ) );
+			Assert.IsTrue( validation.IsSuccess );
+			CompareImageWithReference( "font.png" );
+		}
+
+		[TestMethod]
+		public void RectangleValidationTest()
+		{
+			Dictionary<string, string> variables = new Dictionary<string, string>();
+			Dictionary<string, string> settings = new Dictionary<string, string>();
+
+			File.OpenWrite( "validationsourcefile" ).Close();
+
+			settings.Add( "dest", "dest" );
+			settings.Add( "source", "validationsourcefile" );
+			settings.Add( "rect", "10;10;100;20" );
+
+			ImageWatermark task = new ImageWatermark( settings, variables );
+			OperationResult validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["rect"] = "10;10";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["rect"] = "10;10;10";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Invalid rectangle given: 10;10;10" ) );
+
+			settings["rect"] = "10";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Invalid rectangle given: 10" ) );
+
+			settings["rect"] = "10;10;10;10;10";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Invalid rectangle given: 10;10;10;10;10" ) );
+
+			settings["rect"] = "-10;10";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Negative value given: -10;10" ) );
+
+			settings["rect"] = "10_;10";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Invalid rectangle given: 10_;10" ) );
+		}
+
+		[TestMethod]
+		public void ValidateTest()
+		{
+			Dictionary<string, string> settings = new Dictionary<string, string>();
+			Dictionary<string, string> variables = new Dictionary<string, string>();
+			ImageWatermark task = new ImageWatermark( settings, variables );
+			OperationResult validation = task.Validate();
+
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "No destination file given" ) );
+			Assert.IsTrue( validation.Errors.Contains( "No rectangle given" ) );
+
+			settings.Add( "source", "watermarksource" );
+			if( File.Exists( "watermarksource" ) )
+			{
+				File.Delete( "watermarksource" );
+			}
+
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Source file 'watermarksource' not found" ) );
+			File.OpenWrite( "watermarksource" ).Close();
+
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsFalse( validation.Errors.Contains( "Source file 'watermarksource' not found" ) );
+
+			settings.Add( "dest", "watermarkdest" );
+			settings.Add( "rect", "10;10;100;20" );
+
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings.Add( "font", "Tahoma" );
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["font"] = "Arial:12";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["font"] = "Arial:ab";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Invalid font size specified: ab" ) );
+
+			settings["font"] = "nonexisting";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Font 'nonexisting' is not installed on the system" ) );
+
+			settings["font"] = "Arial";
+			settings["color"] = "blue";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["color"] = "#fff";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["color"] = "#f1f1f1";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["color"] = "#123456";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsTrue( validation.IsSuccess );
+
+			settings["color"] = "test";
+			task = new ImageWatermark( settings, variables );
+			validation = task.Validate();
+			Assert.IsFalse( validation.IsSuccess );
+			Assert.IsTrue( validation.Errors.Contains( "Color 'test' is not a valid color" ) );
+		}
+
+		private static void CompareImageWithReference( string fileName )
+		{
+			string refFileName = Path.Combine( "ReferenceData", fileName );
+
+			Assert.IsTrue( File.Exists( fileName ), "Actual file does not exist" );
+			Assert.IsTrue( File.Exists( refFileName ), "Reference file does not exist" );
+
+			int errorPixels = 0;
+			using( Bitmap refImg = (Bitmap)Bitmap.FromFile( refFileName ) )
+			{
+				using( Bitmap actImg = (Bitmap)Bitmap.FromFile( fileName ) )
+				{
+					Assert.AreEqual( refImg.Width, actImg.Width, "Images are different sized" );
+					Assert.AreEqual( refImg.Height, actImg.Height, "Images are different sized" );
+
+					for( int x = 0; x < refImg.Width; ++x )
+					{
+						for( int y = 0; y < refImg.Height; ++y )
+						{
+							if( refImg.GetPixel( x, y ) != actImg.GetPixel( x, y ) )
+							{
+								++errorPixels;
+							}
+						}
+					}
+				}
+			}
+
+			if( errorPixels > 0 )
+			{
+				Assert.Fail( string.Format( "{0} pixels did not match", errorPixels ) );
+			}
+		}
+	}
+}
---
+++ BumpVersion/BumpVersion/Tasks/ImageWatermark.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace BumpVersion.Tasks
+{
+	/// <summary>
+	/// Task that prints the version number on an image.
+	/// </summary>
+	internal class ImageWatermark : BumpTask
+	{
+		public ImageWatermark( Dictionary<string, string> settings, Dictionary<string, string> variables )
+			: base( settings, variables )
+		{
+		}
+
+		public override OperationResult Bump( Version oldVersion, Version newVersion )
+		{
+			OperationResult result = new OperationResult();
+
+			string sourceFile = GetValue( SourceFileKey );
+			string destFile = GetValue( DestFileKey );
+			Color color = Color.White;
+			string strColor = GetValue( ColorKey );
+			if( strColor != null )
+			{
+				color = ColorTranslator.FromHtml( strColor );
+			}
+			SolidBrush brush = new SolidBrush( color );
+
+			Font font = new Font( "Arial", 12, FontStyle.Regular, GraphicsUnit.Pixel );
+			string strFont = GetValue( FontKey );
+			if( strFont != null )
+			{
+				font.Dispose();
+
+				string name = "";
+				int size = 12;
+
+				int idxSep = strFont.IndexOf( ':' );
+				if( idxSep > 0 )
+				{
+					size = int.Parse( strFont.Substring( idxSep + 1 ) );
+					name = strFont.Substring( 0, idxSep );
+				}
+				else
+				{
+					name = strFont;
+				}
+
+				font = new Font( name, size, FontStyle.Regular, GraphicsUnit.Pixel );
+			}
+
+			string strRect = GetValue( RectangleKey );
+			string[] rectParts = strRect.Split( ';' );
+			bool changeDimensions = false;
+			if( rectParts.Length == 2 )
+			{
+				changeDimensions = true;
+				rectParts = new string[] { rectParts[0], rectParts[1], "0", "0" };
+			}
+
+			int[] rectSizes = rectParts.Select( s => Convert.ToInt32( s ) ).ToArray();
+
+			RectangleF rect = new RectangleF( rectSizes[0], rectSizes[1], rectSizes[2], rectSizes[3] );
+
+			try
+			{
+				using( font )
+				{
+					using( Bitmap sourceImage = (Bitmap)Bitmap.FromFile( sourceFile ) )
+					{
+						using( Bitmap destImage = new Bitmap( sourceImage ) )
+						{
+							if( changeDimensions )
+							{
+								rect.Width = destImage.Width - rect.X;
+								rect.Height = destImage.Height - rect.Y;
+							}
+
+							using( Graphics g = Graphics.FromImage( destImage ) )
+							{
+								g.DrawString( newVersion.ToString(), font, brush, rect );
+							}
+
+							destImage.Save( destFile );
+						}
+					}
+				}
+			}
+			catch( IOException ex )
+			{
+				result.AddError( ex.ToString() );
+			}
+
+			return result;
+		}
+
+		public override OperationResult Validate()
+		{
+			OperationResult result = new OperationResult();
+
+			string source = GetValue( SourceFileKey );
+			if( !File.Exists( source ) )
+			{
+				result.AddError( string.Format( "Source file '{0}' not found", source ) );
+			}
+
+			string dest = GetValue( DestFileKey );
+			if( string.IsNullOrWhiteSpace( dest ) )
+			{
+				result.AddError( "No destination file given" );
+			}
+
+			string rect = GetValue( RectangleKey );
+			if( rect != null )
+			{
+				string[] parts = rect.Split( ';' );
+				if( parts.Length != 2 && parts.Length != 4 )
+				{
+					result.AddError( string.Format( "Invalid rectangle given: {0}", rect ) );
+				}
+
+				if( parts.Length == 2 )
+				{
+					parts = new string[] { parts[0], parts[1], "0", "0" };
+				}
+
+				foreach( string p in parts )
+				{
+					int v;
+					if( !int.TryParse( p, out v ) )
+					{
+						result.AddError( string.Format( "Invalid rectangle given: {0}", rect ) );
+					}
+
+					if( v < 0 )
+					{
+						result.AddError( string.Format( "Negative value given: {0}", rect ) );
+					}
+				}
+			}
+			else
+			{
+				result.AddError( "No rectangle given" );
+			}
+
+			string font = GetValue( FontKey );
+			if( font != null )
+			{
+				int size = 12;
+				int idxSep = font.IndexOf( ':' );
+				bool tryCreate = true;
+
+				if( idxSep >= 0 )
+				{
+					string str = font.Substring( idxSep + 1 );
+					if( !int.TryParse( str, out size ) )
+					{
+						tryCreate = false;
+						result.AddError( string.Format( "Invalid font size specified: {0}", str ) );
+					}
+					else
+					{
+						font = font.Substring( 0, idxSep );
+					}
+				}
+
+				if( tryCreate )
+				{
+					using( Font ft = new Font( font, size, FontStyle.Regular, GraphicsUnit.Pixel ) )
+					{
+						if( ft.Name != font )
+						{
+							result.AddError( string.Format( "Font '{0}' is not installed on the system", font ) );
+						}
+					}
+				}
+			}
+
+			string color = GetValue( ColorKey );
+			if( color != null )
+			{
+				try
+				{
+					ColorTranslator.FromHtml( color );
+				}
+				catch( Exception )
+				{
+					result.AddError( string.Format( "Color '{0}' is not a valid color", color ) );
+				}
+			}
+
+			return result;
+		}
+
+		private const string ColorKey = "color";
+		private const string DestFileKey = "dest";
+		private const string FontKey = "font";
+		private const string RectangleKey = "rect";
+		private const string SourceFileKey = "source";
+	}
+}
